95e853f614f2aa38651e4e8871f8a35931fc6317
NazarStf <hworker98@gmail.com>
2023-07-29 19:22:00 +0300

    Initial Commit

---
+++ Light it Up/Light_it_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Light_it_UpApp.swift
+//  Light it Up
+//
+//  Created by NazarStf on 29.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Light_it_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
